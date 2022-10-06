71c854d5583f5a4bdd1a11ea7bba54df7d932513
Steven Ayvar <ayvarsteven@gmail.com>
2022-10-05 21:39:28 -0500

    adds function that defines the sizing of the pixels on the canvas

--- scripts/app.js
+++ scripts/app.js
@@ -8,3 +8,27 @@ function addColor() {
 };
 
 console.log(canvasPixel);
+
+// ==================== ~ define-pixel-size ====================
+
+// const userInput = prompt(`How many pixels would you like on the canvas?`);
+
+function definePixelSize(userInput) {
+    const canvasWidth = 600;
+    const userPixelInput = 16; // this will be a prompt in the future
+
+    function getRowLength() {
+        return userPixelInput/4;
+    };
+
+    function returnPixelSize() {
+        const rowLength = getRowLength();
+        let pixelSize
+        pixelSize = canvasWidth/rowLength
+        return pixelSize
+    };
+
+    return returnPixelSize();
+}
+
+// ==================== ~ title ====================
