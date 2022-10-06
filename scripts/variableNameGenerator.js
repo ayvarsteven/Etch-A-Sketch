dbcf5dee9a3c716802ba1dc0216fb552e0690657
Steven Ayvar <ayvarsteven@gmail.com>
2022-10-06 14:10:59 -0500

    creates array of variables and generates as many variables as needed

--- scripts/variableNameGenerator.js
+++ scripts/variableNameGenerator.js
@@ -1,17 +1,44 @@
 const pixelClone = `pixelClone`;
 const numOfVariables = 3;
 
+let arrOfVars = [];
+
 function defineName(numOfVariablesGenerator, callFunctionXTimes) { 
     let num = 0;
     for (i = 0; i < numOfVariables; i++) {
         num++
         newName = pixelClone + num
-        console.log(newName)
-        // newName += num
-        // return newName
+        arrOfVars += newName;
+        console.log(arrOfVars);
     }
-    console.log(newName)
+    return arrOfVars
 }
 
 defineName();
-// console.log(defineName())
+
+console.log(arrOfVars);
+
+
+// ==============================================================================================================================
+
+// =============== add-to-window-object =============== 
+
+// for(i = 1; i < 5; i++) { 
+//     window['var_'+i] = + i; 
+// } 
+
+// let pixelClone = `pixelClone`
+
+// =============== use-eval() =============== 
+
+// for (i = 0; i < 16; i++) {
+//     eval('pixelClone_' + i + ' = ' +i);
+// }
+
+// console.log(pixelClone_3)
+
+// console.log(var_1); 
+// console.log(var_2);
+// console.log(var_3);
+// console.log(var_4);
+// console.log(var_5);
